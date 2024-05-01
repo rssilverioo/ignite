a2ef0d01052f53f9166950e6f00ec7a8f936f0d1
rssilverioo <104869030+rssilverioo@users.noreply.github.com>
2024-05-01 11:21:57 -0300

    comment function like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,13 +2,24 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
 
 
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content);
-  } 
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/104869030?v=4" alt="" />
@@ -17,23 +28,23 @@ export function Comment({ content, onDeleteComment }) {
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-                <strong>Rodrigo Silverio</strong>
-                <time title="11 de Maio as 08:13" dateTime="2022-05-11 08:13:30">Publicado cerca de 2hr atras</time>
+              <strong>Rodrigo Silverio</strong>
+              <time title="11 de Maio as 08:13" dateTime="2022-05-11 08:13:30">Publicado cerca de 2hr atras</time>
             </div>
 
             <button title="excluir comentario">
-              <Trash onClick={handleDeleteComment} size={24}/>
+              <Trash onClick={handleDeleteComment} size={24} />
             </button>
           </header>
-          
+
           <p>{content}</p>
         </div>
-          <footer>
-              <button>
-                <ThumbsUp/>
-                Apluadir <span>20</span>
-              </button>
-          </footer>
+        <footer>
+          <button onClick={() => handleLikeComment()}>
+            <ThumbsUp />
+            Apluadir <span>{likeCount}</span>
+          </button>
+        </footer>
       </div>
     </div>
   )
